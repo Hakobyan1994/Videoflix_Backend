24f44e3cef94ad461118ae802dd4d126f4cc1c94
Hakobyan1994 <mesrop.hakobyan@web.de>
2025-09-02 17:05:56 +0200

    addes som files in settings

--- core/settings.py
+++ core/settings.py
@@ -197,7 +197,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
-MEDIA_URL = "/media/"
+
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
