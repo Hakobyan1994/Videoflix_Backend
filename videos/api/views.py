390382adbfb255f702682ccb753bf457476a2dbd
Hakobyan1994 <mesrop.hakobyan@web.de>
2025-09-02 17:15:42 +0200

    addes som files in settings

--- videos/api/views.py
+++ videos/api/views.py
@@ -7,6 +7,10 @@ from rest_framework import status, permissions
 from pathlib import Path
 from django.http import FileResponse, Http404
 from django.conf import settings
+import requests
+from django.http import StreamingHttpResponse, HttpResponseNotFound
+from django.views.decorators.http import require_http_methods
+from django.conf import settings
 
 
 class VideoList(APIView):
@@ -35,22 +39,17 @@ class VideoDetail(APIView):
         serializer = VideoSerializer(video,context={'request': request})
         return Response(serializer.data,status=status.HTTP_200_OK)
 
+@require_http_methods(["GET"])
 def serve_hls(request, video_id: int, suffix: str, filename: str):
-    """
-    Liefert sowohl index.m3u8 als auch jedes segment_xxx.ts
-    → /api/video/<id>/<suffix>/<filename>
-    """
-    file_path = (
-        Path(settings.MEDIA_ROOT) /
-        "videos" / str(video_id) / suffix / filename
-    )
-    if file_path.exists():
-        ctype = (
-            "application/vnd.apple.mpegurl"
-            if filename.endswith(".m3u8")
-            else "video/MP2T"
-        )
-        return FileResponse(file_path.open("rb"), content_type=ctype)
-    raise Http404
+    url = f"{settings.MEDIA_URL}videos/{video_id}/{suffix}/{filename}"
+
+    r = requests.get(url, stream=True)
+    if r.status_code == 404:
+        return HttpResponseNotFound("File not found")
+
+    content_type = "application/vnd.apple.mpegurl" if filename.endswith(".m3u8") else "video/MP2T"
+    response = StreamingHttpResponse(r.iter_content(chunk_size=8192), content_type=content_type)
+    response['Cache-Control'] = 'public, max-age=300'
+    return response
 
 
